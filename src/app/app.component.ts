74302e67bda9e28517d3e703170656f49960ce36
Jan Debowski <>
2025-02-27 18:58:51 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {ParentComponent} from "../components/parent/parent.component";
+import {ChildComponent} from "../components/child/child.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ParentComponent, ChildComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angular-sandbox';
+}
